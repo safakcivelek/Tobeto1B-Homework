0965b55cf5fee5cbf49ccdc41e73082a632e4c53
safakcivelek <safakcivelek3@gmail.com>
2023-11-09 15:12:07 +0300

    Add OOP2-Methods

---
+++ CSharp/OOP2/Methods/CartManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class CartManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : " + product.Name);
+        }
+    }
+}
--- CSharp/OOP2/Methods/Program.cs
+++ CSharp/OOP2/Methods/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Methods:
+
+// Metotlar bizim için tekrar tekrar kullanılabilirliği sağlayan kod bloklarıdır.
+// Dont repeat yourself - DRY - Clean Code - Best Practice
+
+// C#, Java gibi programlama dillerinde herşey class'lardan oluşur. Yani yazdığımız kodlar bu class'ların içerisine yerleştirilir. Tabi çok küçük istisnai configürasyon vs. dosyaları hariçtir. Ama temel yapılar class'ların içerisinde olur.
+using Methods;
+
+string productName = "Elma";
+double Price = 15;
+string description = "Amasya elması";
+
+string[] meyveler = new string[] { "Elma", "Karpuz" };
+
+Product product1 = new Product();
+product1.Name = "Elma";
+product1.Price = 15;
+product1.Description = "Amasya elması";
+
+Product product2 = new Product();
+product2.Name = "Karpuz";
+product2.Price = 80;
+product2.Description = "Diyarbakır karpuzu";
+
+Product[] products = new Product[] { product1, product2 };
+
+foreach (Product product in products)
+{
+    Console.WriteLine(product.Name);
+    Console.WriteLine(product.Price);
+    Console.WriteLine(product.Description);
+    Console.WriteLine("-------------------------");
+}
+
+Console.WriteLine("-----------Metotlar----------");
+
+//instance - örnek
+CartManager cartManager = new CartManager();
+cartManager.Add(product1);
+cartManager.Add(product2);
+
+Console.ReadLine();
